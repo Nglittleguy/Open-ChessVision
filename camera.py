9736511a954ac4bff387bac310f82f1ad2a8affc
Stephen Ng <stephen.ng1999@gmail.com>
2026-08-05 15:20:20 -0400

    added stages

--- camera.py
+++ camera.py
@@ -6,21 +6,40 @@ import numpy as np
 import pandas
 import time
 
+SELECTION_SIZE = 15
+SELECTION_THICKNESS = 2
+FRAME_X = 960
+FRAME_Y = 540
+
 def nothing(x):
     return()
 
-wb_x = 0
-wb_y = 0
+wb_xy = [0, 0]
 wb = (0,0,0)
-mouse_click = False
 
-def mouse_event(event, x, y, flags, params):
-    global wb_y, wb_x, mouse_click
-    if event == cv2.EVENT_LBUTTONDOWN:
-        mouse_click = True
-    if not mouse_click:
-        wb_x = x
-        wb_y = y
+def wb_event(_event, x, y, _flags, _params):
+    global wb_xy
+    wb_xy = [x, y]
+
+selection_stage = 0
+'''
+0: Board
+1: King
+2: Queen
+3: Bishop
+4: Knight
+5: Rook
+6: Pawn
+'''
+
+#            Board,  King, Queen, Bisho, Knigh,  Rook,  Pawn
+sample_xy = [[0,0], [0,0], [0,0], [0,0], [0,0], [0,0], [0,0]]
+sample_hue = [0,        0,     0,     0,     0,     0,     0 ]
+
+
+def sample_event(_event, x, y, _flags, _params):
+    global sample_xy, selection_stage
+    sample_xy[selection_stage] = [x,y]
     
 
 cv2.namedWindow("raw")
@@ -38,17 +57,18 @@ Step 1: Get White balance
 '''
 while rval:
     rval, frame = vc.read()
+    frame_sm = cv2.resize(frame, (FRAME_X, FRAME_Y))
     
 
-    cv2.setMouseCallback('raw', mouse_event)
+    cv2.setMouseCallback('raw', wb_event)
 
     key = cv2.waitKey(10) & 0xFF
 
-    if key == 27 or key == ord('q') or key == ord(' '): # exit on ESC
+    if key == 27 or key == ord('q') or key == ord(' '): # exit on ESC, q, spacebar
         break
 
-    cv2.rectangle(frame, (wb_x-30, wb_y-30), (wb_x+30, wb_y+30), (0, 255, 0), 5)  
-    cv2.imshow("raw", frame)
+    cv2.rectangle(frame_sm, (wb_xy[0]-SELECTION_SIZE, wb_xy[1]-SELECTION_SIZE), (wb_xy[0]+SELECTION_SIZE, wb_xy[1]+SELECTION_SIZE), (255, 255, 255), SELECTION_THICKNESS)  
+    cv2.imshow("raw", frame_sm)
 
 
     # hueValue = cv2.getTrackbarPos("Hue", "Hue Select Window")
@@ -71,7 +91,7 @@ while rval:
 cv2.destroyAllWindows()
 
 '''
-Step 2: White Balanced Loop
+Step 2: Set Up Loop
 '''
 
 # cv2.namedWindow("mask")
@@ -80,23 +100,28 @@ Step 2: White Balanced Loop
 
 while rval:
     rval, frame2 = vc.read()
-    frame2_sm = cv2.resize(frame2, (960, 540))
+    frame2_sm = cv2.resize(frame2, (FRAME_X, FRAME_Y))
     
 
     curr_time = time.time()
-    print(curr_time - last_time)
-    if curr_time - last_time > 5:
+    if curr_time - last_time > 10:
         last_time = curr_time
-        wb = calc_white_balance(frame2, wb_x, wb_y)
+        wb = calc_white_balance(frame2, wb_xy[0], wb_xy[1], SELECTION_SIZE, FRAME_X, FRAME_Y, SELECTION_THICKNESS)
         assert len(wb) == 3
         
     wb_frame = cv2.subtract(frame2_sm, wb)
-    cv2.imshow("White Balanced", wb_frame)
-    cv2.imshow("Non Balanced", frame2_sm)
+    cv2.rectangle(wb_frame, (wb_xy[0]-SELECTION_SIZE, wb_xy[1]-SELECTION_SIZE), (wb_xy[0]+SELECTION_SIZE, wb_xy[1]+SELECTION_SIZE), (255, 255, 255), SELECTION_THICKNESS)  
 
+
+    cv2.setMouseCallback('White Balanced', sample_event)
+
+    for stage in range(selection_stage + 1):
+        cv2.rectangle(frame2_sm, (sample_xy[stage][0]-SELECTION_SIZE, sample_xy[stage][1]-SELECTION_SIZE), (sample_xy[stage][0]+SELECTION_SIZE, sample_xy[stage][1]+SELECTION_SIZE), (0, 0, 255), SELECTION_THICKNESS)  
+
+    cv2.imshow("White Balanced", wb_frame)
     key = cv2.waitKey(10) & 0xFF
 
-    if key == 27 or key == ord('q') or key == ord(' '): # exit on ESC
+    if key == ord(' '): # next on spacebar
         break
     
     
--- white_balance.py
+++ white_balance.py
@@ -3,20 +3,20 @@ import cv2
 
 #Frame coming in is BRG numpy.ndarray
 
-def calc_white_balance(frame, wb_x, wb_y):
-    assert wb_y > 30 and wb_x > 30 and wb_y < 1050 and wb_x < 1890, f"ERROR: Outside camera bounds: x:{wb_x}, y:{wb_y}"
+def calc_white_balance(frame, wb_x, wb_y, selection_size, frame_x, frame_y, thickness):
+    assert wb_y > selection_size and wb_x > selection_size and wb_y < frame_y-selection_size and wb_x < frame_x-selection_size, f"ERROR: Outside camera bounds: x:{wb_x}, y:{wb_y}"
 
     wb_r = 0
     wb_g = 0
     wb_b = 0
 
-    cc_roi = frame[wb_y-25:wb_y+25, wb_x-25:wb_x+25]
+    cc_roi = frame[wb_y-selection_size-thickness:wb_y+selection_size-thickness, wb_x-selection_size-thickness:wb_x+selection_size-thickness]
     cv2.imshow("Color Correction (Pre)", cc_roi)
 
-    for x in range(50):
-        for y in range(50):
+    for x in range(2*selection_size-thickness):
+        for y in range(2*selection_size-thickness):
             colors = cc_roi[x, y]
-            colors_avg = list(map(lambda a: float(a)/2500, colors))
+            colors_avg = list(map(lambda a: float(a)/(selection_size-thickness)/(selection_size-thickness), colors))
             wb_r = wb_r + colors_avg[0]
             wb_g = wb_g + colors_avg[1]
             wb_b = wb_b + colors_avg[2]
