77fff66e4fd3a909a03bfe004c4bd28bee59418e
Stephen Ng <stephen.ng1999@gmail.com>
2026-08-07 11:39:59 -0400

    prevent failure

--- hue_picker.py
+++ hue_picker.py
@@ -7,6 +7,10 @@ def calc_hue(frame, x, y, selection_size, frame_x, frame_y, thickness):
     if y > selection_size and x > selection_size and y < frame_y-selection_size and x < frame_x-selection_size:
 
         hue_roi = frame[y-selection_size-thickness:y+(selection_size-thickness), x-selection_size-thickness:x+(selection_size-thickness)]
+
+        if len(hue_roi) == 0 or len(hue_roi[0]) == 0 or len(hue_roi[0][0]) == 0:
+            return 0
+        
         hue_roi_hsv = cv2.cvtColor(hue_roi, cv2.COLOR_BGR2HSV)
         h, _s, _v, _ = cv2.mean(hue_roi_hsv)
 
