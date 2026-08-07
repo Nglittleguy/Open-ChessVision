ddd373316e374cb25dd46e2776dc6a89bece07b8
Stephen Ng <stephen.ng1999@gmail.com>
2026-08-07 14:52:15 -0400

    radian based hue picker

---
+++ hue_picker_rad.py
@@ -0,0 +1,27 @@
+import numpy as np
+import cv2
+
+RED_THRESHOLD = 10
+
+#Frame coming in is BRG numpy.ndarray -> translate to HSV to get H
+
+def calc_hue(frame, x, y, selection_size, frame_x, frame_y, thickness):
+    if y > selection_size and x > selection_size and y < frame_y-selection_size and x < frame_x-selection_size:
+
+        hue_roi = frame[y-selection_size-thickness:y+(selection_size-thickness), x-selection_size-thickness:x+(selection_size-thickness)]
+        if len(hue_roi) == 0 or len(hue_roi[0]) == 0 or len(hue_roi[0][0]) == 0:
+            return 0
+        
+        hue_roi_hsv = cv2.cvtColor(hue_roi, cv2.COLOR_BGR2HSV)
+        hues = hue_roi_hsv[:, 0]
+        hue_rad = hues * (2.0 * np.pi / 180.0)
+
+        mean_sin = np.mean(np.sin(hue_rad))
+        mean_cos = np.mean(np.cos(hue_rad))
+        mean_hue_rad = np.arctan2(mean_sin, mean_cos)
+
+        mean_hue = (mean_hue_rad * 180.0 / (2.0 * np.pi)) % 180.0
+        print(mean_hue)
+        return mean_hue
+    else:
+        return 0
